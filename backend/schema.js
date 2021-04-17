6beaacd36268833c61b098066b5dcd751506b8d8
Ritesh T <riteshdthakur@gmail.com>
2021-04-17 22:58:36 +0530

    Updated schema.js with properties

--- backend/schema.js
+++ backend/schema.js
@@ -4,8 +4,16 @@ const Schema = mongoose.Schema;
 
 // Create a custom blog schema
 const blogSchema = new Schema({
-    title: String,
-    content: String,
+    title: {
+        type: String,
+        required: true,
+        minlength: 5,
+    },
+    content: {
+        type: String,
+        required: true,
+        minlength: 20,
+    },
 });
 
 // Export the created schema as a model
