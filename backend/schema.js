f4417f5cff9a0d853e22bc74aa2708ebbc8ad3af
Ritesh T <riteshdthakur@gmail.com>
2021-04-17 22:39:05 +0530

    Added comments

--- backend/schema.js
+++ backend/schema.js
@@ -1,12 +1,12 @@
+// Import mongoose and Schema
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+// Create a custom blog schema
 const blogSchema = new Schema({
     title: String,
-
     content: String,
-
-    // image: String
 });
 
+// Export the created schema as a model
 module.exports = mongoose.model("Blogs", blogSchema);
--- backend/server.js
+++ backend/server.js
@@ -1,80 +1,104 @@
+// Import Express and CORS
 const express = require("express");
 const cors = require("cors");
+
+// Import mongoose
 const mongoose = require("mongoose");
 
+// Import Blog schema
+const Blog = require("./schema");
+
+// MongoDB Database URL
 const uri = 'mongodb+srv://merncrud:merncrud@cluster0.bjt7a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
-// mongodb+srv://merncrud:merncrud@cluster0.3vk8f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
-
+// Connecting to MongoDB
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
 const conn = mongoose.connection;
 conn.once("open", () => {
     console.log("MongoDB connected");
-})
+});
 
-const Blog = require("./schema");
-
+// Create a Express app
 const app = express();
+
+// Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use(express.json());
+
+// Start the server on port 8000
 app.listen(8000, () => {
     console.log("Server listening on port 8000");
 });
 
+// POST endpoint to create a blog
 app.post("/create", (req, res) => {
-    // Create blogs
+    // Get title and content from request body
     title = req.body.title;
     content = req.body.content;
 
+    // Create blog using create()
     Blog.create({
         title: title,
         content: content,
     })
         .then((blog) => {
+            // Send the created blog
             res.status(200).json(blog);
         })
+        // Return 400 response and error message
         .catch(err => res.status(400).json("Error:" + err));
 });
 
+// GET endpoint to get all blogs
 app.get("/home", (req, res) => {
-    // Read blogs
+    // Get all blogs
     Blog.find()
         .then((blogs) => {
+            // Send the blogs as JSON
             res.status(200).json(blogs);
         })
         .catch((err) => res.status(400).json("Error:" + err));
 });
 
+// GET endpoint to get a blog by it's id
 app.get('/blog/:id', (req, res) => {
+    // Get blog by _id
     Blog.findById({ _id: req.params.id })
-      .then((result) => res.json(result))
-      .catch((err) => res.status(400).json("Error: " + err));
+        .then((result) => res.json(result))
+        .catch((err) => res.status(400).json("Error: " + err));
 });
 
+// PUT endpoint to edit a blog by it's id
 app.put("/edit/:id", (req, res) => {
-    // Update blogs
+    // Get title, content and _id from request body
     title = req.body.title;
     content = req.body.content;
     blogId = req.params.id;
 
+    // Find and update blog by it's _id
     Blog.findByIdAndUpdate(req.params.id, {
         title: title,
         content: content
     })
         .then((data) => {
-            res.status(200).json(data); 
+            // Send updated blog as JSON
+            res.status(200).json(data);
         })
         .catch((err) => res.status(400).json("Error:" + err));
 });
 
+
+// DELETE endpoint to delete blog by id
 app.delete("/delete/:id", (req, res) => {
-    // Delete blogs
+    // Get _id from request body
     blogId = req.params.id;
 
+    // Find and delete blog by it's _id
     Blog.findByIdAndDelete(req.params.id)
         .then((data) => {
-            res.status(200).json(data); 
+            // Send deleted blog as JSON
+            res.status(200).json(data);
         })
         .catch((err) => res.status(400).json("Error:" + err));
-}); 
+});
